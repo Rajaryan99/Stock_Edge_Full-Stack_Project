f43ba240c6b0b72e3dbbdb8182b507a62b325d89
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-31 12:49:28 +0530

    fix: remove submodule and add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './Name'
+import Name from './Name';
+
+function App() {
+  return (
+    <>
+    <Name/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Name.js
@@ -0,0 +1,9 @@
+ function Name() {
+    return (
+        <div>
+            <h1>Raj Aryan</h1>
+        </div>
+    )
+ }
+
+ export default Name;
