b08e7c51a2698ec8a00cb16c35b8061a9c8801dc
Rajaryan singh <raj.aryan2518@gmail.com>
2025-11-05 23:48:05 +0530

    landing page

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,12 +1,12 @@
 function Hero() {
     return ( 
             <section className='container-fluid'id="supportHero">
-            <div className=' p-5  ' id="supportwrapper">
-                <h3 className="">Support Portal</h3>
-                <a href="">Track Ticket</a>
+            <div className=' p-3  ' id="supportwrapper">
+                <h3 className=" mt-4">Support Portal</h3>
+                <a href="" className="mt-4 pe-5">Track Ticket</a>
             </div> 
-            <div className=" row p-5 ">
-                <div className="col-5 p-5  " id="supportSearch">
+            <div className=" row p-3 ">
+                <div className="col-5 p-3 ps-5 " id="supportSearch">
                     <h1 className="fs-4  mb-4">Search for an answer or browse help topic to create a ticket</h1>
                     <input type="text" class="input-field" placeholder="Eg. how do I activate F&Q"/> <br/>
                     <a href="">Track account opening</a> &nbsp;
@@ -15,10 +15,10 @@ function Hero() {
                     <a href="">Kite user manual</a>
                 </div>
                 <div className="col-2"></div>
-                <div className="col-5 p-5 " id="supportSearch">
+                <div className="col-5 p-3 ps-5 " id="supportSearch">
                     <h1 className="fs-4 mb-4">Feature</h1>
-                    <ol>
-                        <li><a href="" className="">Current Takeover and Delisting - January 2025</a></li> <br/>
+                    <ol className="mb-5">
+                        <li ><a href="" className="">Current Takeover and Delisting - January 2025</a></li> <br/>
                         <li><a href="" className="">Latest Intraday leverage -MIS & CO</a></li>
                     </ol>
                 </div>
