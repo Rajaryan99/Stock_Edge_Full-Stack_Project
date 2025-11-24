90faba61f74b41b5a6bb1373fb2f09bd8cd6b08a
Raj/aryan <raj.aryan2518@gmail.com>
2025-11-24 17:12:30 +0530

    Update Login.js

--- frontend/src/landing_page/pages/Login.js
+++ frontend/src/landing_page/pages/Login.js
@@ -42,7 +42,7 @@ const Login = () => {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-         window.location.href = "https://zerodha-clone-dashboard-c2fd.onrender.com"
+         window.location.replace("https://zerodha-clone-dashboard-c2fd.onrender.com")
         }, 1000);
       } else {
         handleError(message);
